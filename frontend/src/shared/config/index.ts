f492c68d7f2a23d493a8fedf7e968ae92a685b50
Roman Kaluger <r.kaluger@sftpro.info>
2025-08-07 00:13:31 +0300

    build

--- frontend/src/shared/config/index.ts
+++ frontend/src/shared/config/index.ts
@@ -1,5 +1,7 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3001/api";
+  process.env.NODE_ENV === "production"
+    ? "/api"
+    : process.env.REACT_APP_API_URL || "http://localhost:3001/api";
 
 export const ROUTES = {
   HOME: "/",
