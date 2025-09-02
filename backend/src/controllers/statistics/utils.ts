3fc382b34bcd449c0c8d58d7320bb3aaa2b37366
Roman Kaluger <r.kaluger@sftpro.info>
2025-09-02 22:52:46 +0600

    fix date

--- backend/src/controllers/statistics/utils.ts
+++ backend/src/controllers/statistics/utils.ts
@@ -1,11 +1,27 @@
 export const getDateRange = (startDate?: string, endDate?: string) => {
   const now = new Date();
   const currentMonthStart = new Date(now.getFullYear(), now.getMonth(), 1);
-  const currentMonthEnd = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+  const currentMonthEnd = new Date(
+    now.getFullYear(),
+    now.getMonth() + 1,
+    0,
+    23,
+    59,
+    59,
+    999
+  );
+
+  const lte = endDate
+    ? (() => {
+        const d = new Date(endDate);
+        d.setHours(23, 59, 59, 999);
+        return d;
+      })()
+    : currentMonthEnd;
 
   return {
     gte: startDate ? new Date(startDate) : currentMonthStart,
-    lte: endDate ? new Date(endDate) : currentMonthEnd,
+    lte,
   };
 };
 
